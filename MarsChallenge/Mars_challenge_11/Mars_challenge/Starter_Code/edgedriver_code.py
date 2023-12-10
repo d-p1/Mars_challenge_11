d23a312af10a84674f649d0c08919ec312b088ea
Diana Rodriguez <134551608+d-p1@users.noreply.github.com>
2023-12-09 21:42:11 -0800

    Update edgedriver_code.py

--- MarsChallenge/Mars_challenge_11/Mars_challenge/Starter_Code/edgedriver_code.py
+++ MarsChallenge/Mars_challenge_11/Mars_challenge/Starter_Code/edgedriver_code.py
@@ -1,28 +1,16 @@
-##because of technical difficulties with chrome, i attempted to do the assignment using edge.
-##These are my notes, and attempt
-##this file is not part of the assignment.
-from msedge.selenium_tools import Edge, EdgeOptions
+##because of technical difficulties with chrome, I attempted to do the assignment using edge.
+##These are my notes and attempt.
+##This file is not part of the assignment. But can help if attempting this challenge :)
+# Import Splinter and BeautifulSoup
+from splinter import Browser
 from bs4 import BeautifulSoup as soup
+from selenium import webdriver
+
 
 # Set the path to the Microsoft Edge WebDriver executable
 edge_driver_path = r'C:\Users\msedgedriver.exe'
+executable_path = {'executable_path': edge_driver_path}
 
-# Configure the Edge WebDriver options
-options = EdgeOptions()
-options.use_chromium = True  # Use Chromium-based Edge
+# Open Edge Browser 
 
-# Create the WebDriver instance
-browser = Edge(executable_path=edge_driver_path, options=options)
-
-# Visit the desired URL
-url = 'https://static.bc-edx.com/data/web/mars_news/index.html'
-browser.get(url)
-
-# For example, you can get the page source using BeautifulSoup
-page_source = soup(browser.page_source, 'html.parser')
-
-# Print the title as a check
-print(page_source.title)
-
-# Don't forget to close the browser when you're done
-browser.quit()
+browser = Browser('edge')
